24a35b0c2e37a5a00b969087058bd8163d796950
Ezra <97122016+ezra-casas@users.noreply.github.com>
2023-04-02 22:43:40 -0700

    Refactored quiz/+page.svelte. Moved functions to helpers folder

--- src/lib/helpers/helpersFunctions.ts
+++ src/lib/helpers/helpersFunctions.ts
@@ -20,3 +20,51 @@ export function selectAll(e:Event){
         selectedKanaGroup.update(array => [...array, box.value])
     })
 } 
+
+export function handleBack(){
+    selectedKanaGroup.set([])
+    goto("/")
+}
+
+function getRandomIndex(max: number): number {
+    return Math.floor(Math.random() * max)
+}
+
+export function print(str:Object){
+    console.group(str)
+}
+
+export function addRandomKana(kanaKeys: string[], kanaValues: string[][], selectedKana: string[], selectedValues:string[][]): void {
+    const randomIndex = getRandomIndex(kanaKeys.length)
+    const selectedKey = kanaKeys[randomIndex]
+    const selectedValue = kanaValues[randomIndex]
+
+    selectedValues.push(selectedValue)
+    selectedKana.push(selectedKey)
+}
+
+
+export function randomizeKana(kanaGroupsToSelectFrom, selectedValues:string[][], hiraganaArray, katakanaArray ): string[] {
+    let kanaGroup: string[] = [];
+
+    while (kanaGroup.length < 10) {
+        for (let i: number = 0; i < kanaGroupsToSelectFrom.length; i++) {
+            const group: string = kanaGroupsToSelectFrom[i];
+            const groupFirstLetter: string = group[0];
+            const isLessThanKanaGroup: boolean = kanaGroup.length < 10;
+
+            if (groupFirstLetter === "h" && isLessThanKanaGroup) {
+                const currentKeys: string[] = Object.keys(hiraganaArray[kanaGroupsToSelectFrom[i]]);
+                const currentValues: string[][] = Object.values(hiraganaArray[kanaGroupsToSelectFrom[i]]);
+                addRandomKana(currentKeys, currentValues, kanaGroup, selectedValues)
+            }
+
+            if (groupFirstLetter === "k" && isLessThanKanaGroup) {
+                const currentKeys: string[] = Object.keys(katakanaArray[kanaGroupsToSelectFrom[i]]);
+                const currentValues: string[][] = Object.values(katakanaArray[kanaGroupsToSelectFrom[i]]);
+                addRandomKana(currentKeys, currentValues, kanaGroup, selectedValues)
+            }
+        }
+    }
+    return kanaGroup;
+}
--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -1,23 +1,26 @@
 <script lang="ts">
-    import { selectedKanaGroup, romajiAnswers } from "../../stores";
+    import { 
+        selectedKanaGroup, 
+        romajiAnswers 
+    } from "../../stores";
     import { kana } from "../../data/dictionary";
-	import { goto } from "$app/navigation";
+    import {
+        handleBack, 
+        print, 
+        addRandomKana, 
+        randomizeKana
+    } from "../../lib/helpers/helpersFunctions"
     
     const [hiraganaArray, katakanaArray] = kana;
-    const availableHiragana = Object.keys(hiraganaArray);
-    const availableKatakana = Object.keys(katakanaArray);
     
-    let values:string[] = [];
+    let selectedValues:string[][] = [];
     let guess: string = "";
-    let currentIndex: number = 0;
+    let currentIndex = 0;
 
-    function print(str){
-        console.group(str)
-    }
-    const questions = randomizeKana()
+    const questions = randomizeKana($selectedKanaGroup, selectedValues, hiraganaArray, katakanaArray)
     
     function handleClick(){
-        if(values[currentIndex].includes(guess)){
+        if(selectedValues[currentIndex].includes(guess)){
             console.log("pass")
             currentIndex++
             guess = ""
@@ -27,49 +30,6 @@
             guess = ""
         }
         
-    }
-    function addKana(currentKeys: string[], currentValues: string[], kanaGroup:string[]): void{
-        let currentElementIndex: number = Math.floor(Math.random() * currentKeys.length)
-        const currentKey: string = currentKeys[currentElementIndex]
-        const currentValue: string[] = currentValues[currentElementIndex]
-        values.push(currentValue)
-
-        if(kanaGroup.includes(currentKey)){
-            currentElementIndex = Math.floor(Math.random() * currentKeys.length)
-        }
-        kanaGroup.push(currentKey)
-    }
-
-    function randomizeKana(): string[] {
-        let kanaGroup: string[] = [];
-
-        while (kanaGroup.length < 10) {
-            for (let i: number = 0; i < $selectedKanaGroup.length; i++) {
-                const group: string = $selectedKanaGroup[i];
-                
-                const groupFirstLetter: string = group[0];
-                const isLessThanKanaGroup: boolean = kanaGroup.length < 10;
-                
-
-                if (groupFirstLetter === "h" && isLessThanKanaGroup) {
-                    const currentKeys: string[] = Object.keys(hiraganaArray[$selectedKanaGroup[i]]);
-                    const currentValues: string[] = Object.values(hiraganaArray[$selectedKanaGroup[i]]);
-                    addKana(currentKeys, currentValues, kanaGroup)
-                }
-
-                if (groupFirstLetter === "k" && isLessThanKanaGroup) {
-                    const currentKeys: string[] = Object.keys(katakanaArray[$selectedKanaGroup[i]]);
-                    const currentValues: string[] = Object.values(katakanaArray[$selectedKanaGroup[i]]);
-                    addKana(currentKeys, currentValues, kanaGroup)
-                }
-            }
-        }
-        return kanaGroup;
-    }
-
-    function handleBack(){
-        $selectedKanaGroup.length = 0
-        goto("/")
     }
 
 </script>
